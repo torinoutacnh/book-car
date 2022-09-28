f5fa1389038d931f3eb074f01b46db36b02c81c6
Tinh Quan <89092556+ntquan612@users.noreply.github.com>
2022-09-28 14:57:13 +0700

    update

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,25 +1,64 @@
+import { dateTimePickerTabsClasses } from "@mui/x-date-pickers";
+import { User } from "interface/authenticate";
 import React from "react";
-import { GoogleLogin } from "react-google-login";
-
-const clientId = "681359121878-soqrndjm3aiqh3rlgebrv4orkqn7gvfo.apps.googleusercontent.com";
+import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
 
 function Login() {
-    const onSuccess = (res: any) => {
-        console.log("ĐĂNG NHẬP THÀNH CÔNG ! XIN CHÀO ", res.profileObj);
-    };
-    const onFailure = (res: any) => {
-        console.log("ĐĂNG NHẬP THẤT BẠI ! LỖI: ", res);
-    };
-
+    async function onSuccess(
+        response: GoogleLoginResponse | GoogleLoginResponseOffline
+    ): Promise<User> {
+        const gbj = (response as GoogleLoginResponse).profileObj;
+        const res = await fetch(process.env.REACT_APP_API + "/user/authenticate", {
+            method: "POST",
+            headers: { "Content-Type": "application/json", Authorization: "Bearer my-token" },
+            body: JSON.stringify({
+                userName: gbj.email,
+                password: gbj.googleId,
+            }),
+        });
+        if (res.status === 404) {
+            const createUser = await fetch(process.env.REACT_APP_API + "/user/create", {
+                method: "POST",
+                headers: { "Content-Type": "application/json", Authorization: "Bearer my-token" },
+                body: JSON.stringify({
+                    name: gbj.name,
+                    tel: "",
+                    job: "",
+                    currentAddress: "",
+                    email: gbj.email,
+                    passWord: gbj.googleId.substring(0, 20),
+                    citizenIdentificationInfo: {
+                        number: "",
+                        address: "",
+                        dateReceive: new Date(),
+                    },
+                    passportInfo: {
+                        number: "",
+                        address: "",
+                        dateReceive: new Date(),
+                    },
+                    drivingLicenseInfo: {
+                        number: "",
+                        dateReceive: new Date(),
+                    },
+                }),
+            });
+            console.log(gbj);
+        }
+        const data = await res.json();
+        console.log(res.status);
+        console.log(data);
+        return data;
+    }
     return (
         <div id="signInButton">
             <GoogleLogin
-                clientId={clientId}
+                clientId={process.env.REACT_APP_CLIENT_ID || ""}
                 buttonText="Đăng nhập"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
+                onSuccess={(res) => onSuccess(res)}
+                onFailure={(err: any) => {}}
                 cookiePolicy={"single_host_origin"}
-                isSignedIn={true}
+                isSignedIn={false}
             />
         </div>
     );
