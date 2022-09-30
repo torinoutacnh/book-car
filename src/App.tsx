cc05807f4390721380a2e12f89c93e969354d18e
Tinh Quan <89092556+ntquan612@users.noreply.github.com>
2022-09-30 16:48:41 +0700

    add page contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import ListCar from "pages/listCar/ListCar";
-import Home from "pages/home";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Home from "pages/home";
+import ListCar from "pages/listCar/ListCar";
 import Assessment from "pages/assessment/Assessment";
+import Contract from "pages/contract/Contract";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/danh-sach-xe" element={<ListCar />} />
                 <Route path="/tham-dinh" element={<Assessment />} />
+                <Route path="/thong-tin-ca-nhan" element={<Contract />} />
             </Routes>
         </div>
     );
