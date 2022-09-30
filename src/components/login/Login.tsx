d5b7ac4156569727606b62ab2f741d11ebcc7998
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2022-09-30 13:35:02 +0700

    h

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,67 +1,33 @@
-import { dateTimePickerTabsClasses } from "@mui/x-date-pickers";
-import { User } from "interface/authenticate";
-import React from "react";
-import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
+import AuthContext from "context/AuthContext";
+import { gapi } from "gapi-script";
+import { useContext, useEffect } from "react";
+import { GoogleLogin, GoogleLoginResponse } from "react-google-login";
 
 function Login() {
-    async function onSuccess(
-        response: GoogleLoginResponse | GoogleLoginResponseOffline
-    ): Promise<User> {
-        const gbj = (response as GoogleLoginResponse).profileObj;
-        const res = await fetch(process.env.REACT_APP_API + "/user/authenticate", {
-            method: "POST",
-            headers: { "Content-Type": "application/json", Authorization: "Bearer my-token" },
-            body: JSON.stringify({
-                userName: gbj.email,
-                password: gbj.googleId,
-            }),
-        });
-        if (res.status === 404) {
-            const createUser = await fetch(process.env.REACT_APP_API + "/user/create", {
-                method: "POST",
-                headers: { "Content-Type": "application/json", Authorization: "Bearer my-token" },
-                body: JSON.stringify({
-                    name: gbj.name,
-                    tel: "",
-                    job: "",
-                    currentAddress: "",
-                    email: gbj.email,
-                    passWord: gbj.googleId.substring(0, 20),
-                    citizenIdentificationInfo: {
-                        number: "",
-                        address: "",
-                        dateReceive: new Date(),
-                    },
-                    passportInfo: {
-                        number: "",
-                        address: "",
-                        dateReceive: new Date(),
-                    },
-                    drivingLicenseInfo: {
-                        number: "",
-                        dateReceive: new Date(),
-                    },
-                }),
-            });
-            console.log(gbj);
-        }
-        const data = await res.json();
-        console.log(res.status);
-        console.log(data);
-        return data;
-    }
-    return (
-        <div id="signInButton">
-            <GoogleLogin
-                clientId={process.env.REACT_APP_CLIENT_ID || ""}
-                buttonText="Đăng nhập"
-                onSuccess={(res) => onSuccess(res)}
-                onFailure={(err: any) => {}}
-                cookiePolicy={"single_host_origin"}
-                isSignedIn={false}
-            />
-        </div>
-    );
+  const { authen } = useContext(AuthContext);
+
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        scope: "",
+      });
+    };
+    gapi.load("client:auth2", initClient);
+  });
+
+  return (
+    <div id="signInButton">
+      <GoogleLogin
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        buttonText="Đăng nhập"
+        onSuccess={async (res) => await authen(res as GoogleLoginResponse)}
+        onFailure={(err: any) => {}}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={false}
+      />
+    </div>
+  );
 }
 
 export default Login;
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,84 +1,77 @@
-import React, { createContext, useState, useEffect } from "react";
-import { gapi } from "gapi-script";
-import { GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
-import { User } from "interface/authenticate";
-// interface Con{
-//     auth: any;
-//     A:JSX.Element
-// }
+import { createContext, useState } from "react";
+import { GoogleLoginResponse } from "react-google-login";
 
 const AuthContext = createContext<any>(null);
 
-// const onSuccess = (res: any) => {
-//     console.log("ĐĂNG NHẬP THÀNH CÔNG ! XIN CHÀO ", res.profileObj);
-// };
-// const onFailure = (res: any) => {
-//     console.log("ĐĂNG NHẬP THẤT BẠI ! LỖI: ", res);
-// };
+function AuthProvider(props: { children: any }) {
+  const [auth, setAuth] = useState();
 
-const clientId = process.env.REACT_APP_CLIENT_ID || "";
-
- function AuthProvider(props: { children: any }) {
-    const [auth, setAuth] = useState();
-    
-    async function onSuccess(
-        response: GoogleLoginResponse | GoogleLoginResponseOffline
-    ): Promise<User> {
-        const gbj = (response as GoogleLoginResponse).profileObj;
-        const res = await fetch(process.env.REACT_APP_API + "/user/authenticate", {
-            method: "POST",
-            headers: { "Content-Type": "application/json", Authorization: "Bearer my-token" },
-            body: JSON.stringify({
-                userName: gbj.email,
-                password: gbj.googleId,
-            }),
-        });
-        if (res.status === 404) {
-            const createUser = await fetch(process.env.REACT_APP_API + "/user/create", {
-                method: "POST",
-                headers: { "Content-Type": "application/json", Authorization: "Bearer my-token" },
-                body: JSON.stringify({
-                    name: gbj.name,
-                    tel: "",
-                    job: "",
-                    currentAddress: "",
-                    email: gbj.email,
-                    passWord: gbj.googleId.substring(0, 20),
-                    citizenIdentificationInfo: {
-                        number: "",
-                        address: "",
-                        dateReceive: new Date(),
-                    },
-                    passportInfo: {
-                        number: "",
-                        address: "",
-                        dateReceive: new Date(),
-                    },
-                    drivingLicenseInfo: {
-                        number: "",
-                        dateReceive: new Date(),
-                    },
-                }),
-            });
-            console.log(gbj);
-        }
-        const data = await res.json();
-        console.log(res.status);
-        console.log(data);
-        return data;
-    }
-
-    useEffect(() => {
-        const initClient = () => {
-            gapi.client.init({
-                clientId: clientId,
-                scope: "",
-            });
-        };
-        gapi.load("client:auth2", initClient);
+  const authen = async (response: GoogleLoginResponse) => {
+    var res = await fetch(process.env.REACT_APP_API + "/user/authenticate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userName: response.profileObj.email,
+        password: response.profileObj.googleId.substring(0, 20),
+      }),
     });
 
-    return <AuthContext.Provider value={{ auth }}>{props.children}</AuthContext.Provider>;
+    if (res.status === 404) {
+      const user = await createUser(response);
+      return setAuth(user);
+    }
+
+    const data = await res.json();
+    if (res.status >= 400 && res.status !== 404) {
+      throw new Error(`${res.status}:${data.message}`);
+    }
+
+    return setAuth(data);
+  };
+
+  const createUser = async (response: GoogleLoginResponse) => {
+    const user = {
+      name: response.profileObj.name,
+      tel: "",
+      job: "",
+      currentAddress: "",
+      email: response.profileObj.email,
+      passWord: response.profileObj.googleId.substring(0, 20),
+      citizenIdentificationInfo: {
+        number: "",
+        address: "",
+        dateReceive: new Date(),
+      },
+      passportInfo: {
+        number: "",
+        address: "",
+        dateReceive: new Date(),
+      },
+      drivingLicenseInfo: {
+        number: "",
+        dateReceive: new Date(),
+      },
+    };
+
+    const res = await fetch(process.env.REACT_APP_API + "/user/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer my-token",
+      },
+      body: JSON.stringify(user),
+    });
+
+    const data = await res.json();
+    if (res.status >= 400) throw new Error(`${res.status}:${data.message}`);
+    return data;
+  };
+
+  return (
+    <AuthContext.Provider value={{ auth, authen }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
 
 export default AuthContext;
